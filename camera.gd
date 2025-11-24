5bcb2dedb04ed0b33f2f6f6475d89c955a3f7986
Ferran Ruiz Sala <ferriruiz3@gmail.com>
2025-11-24 19:01:15 +0100

    Create camera that follows main character

---
+++ camera.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+@export var player : Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	
+	global_position = lerp(global_position, player.global_position, 0.1)
