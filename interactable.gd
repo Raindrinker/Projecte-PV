34ed66d55a1ad5863cbfff02cf026e35f80ffed7
Marta Gras Guilamany <mgrasg01@gmail.com>
2025-12-02 18:30:11 +0100

    add paredes

--- interactable.gd
+++ interactable.gd
@@ -7,14 +7,13 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Personatge:
 		body.select_interactable = self
 		modulate = Color.YELLOW
-		print("cartel entra")
 			
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is Personatge:
 		if body.select_interactable == self:
 			body.select_interactable = null
 			modulate = Color.WHITE
-			print("cartel surt")
+			
 	
 func interact(player):
-	print("interact")
+	pass
