425cbf801728ea0aa9db8ef3bd3a0b612b84de25
dakotagh04 <dakota.gazquez@gmail.com>
2026-01-11 20:29:06 +0100

    add camera limits

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,28 +1,57 @@
 extends Camera2D
 
-@export var player : Node2D
+@export var player: Node2D
 
-var tshake = 0
+# --- SALA 1 ---
+@export var sala1_left: int = 0
+@export var sala1_right: int = 2000
 
-var shake_offset = Vector2.ZERO
+# --- SALA 2 ---
+@export var sala2_left: int = 2000
+@export var sala2_right: int = 4096
 
-# Called when the node enters the scene tree for the first time.
+# Límites verticales (comunes)
+@export var limit_top_manual: int = 0
+@export var limit_bottom_manual: int = 2048
+
+var tshake := 0.0
+var shake_offset := Vector2.ZERO
+
 func _ready() -> void:
-	pass # Replace with function body.
+	enabled = true
+	set_sala_1()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
-	if tshake > 0:
-		shake_offset = Vector2.DOWN.rotated(randf_range(0, 2*PI)) * randf_range(0, 1) * 5
+	update_sala_by_player()
+
+	if tshake > 0.0:
+		shake_offset = Vector2.DOWN.rotated(randf_range(0.0, TAU)) * randf_range(0.0, 1.0) * 5.0
 		tshake -= delta
 	else:
 		shake_offset = Vector2.ZERO
-	
-	
+
 	global_position = lerp(global_position, player.global_position, 0.1) + shake_offset
 
+# ------------------------------------------------
+
+func update_sala_by_player() -> void:
+	# Cambia esto según dónde esté el corte entre salas
+	if player.global_position.x < sala2_left:
+		set_sala_1()
+	else:
+		set_sala_2()
+
+func set_sala_1() -> void:
+	limit_left = sala1_left
+	limit_right = sala1_right
+	limit_top = limit_top_manual
+	limit_bottom = limit_bottom_manual
+
+func set_sala_2() -> void:
+	limit_left = sala2_left
+	limit_right = sala2_right
+	limit_top = limit_top_manual
+	limit_bottom = limit_bottom_manual
 
 func _on_personatge_take_damage() -> void:
 	tshake = 0.2
